ac43568b23d7ee3ff999b5fd111b07ef46c3d47f
pragneya joshi <jpragney@gmail.com>
2026-02-15 06:16:57 +0530

    Update trapezoidal-rule-for-exisitng-files.py

--- mark-neuman-implementations/trapezoidal-rule-for-exisitng-files.py
+++ mark-neuman-implementations/trapezoidal-rule-for-exisitng-files.py
@@ -1,15 +1,15 @@
-#FOR A FILE BASED ONLY
 import numpy as np
 import matplotlib.pyplot as plt
 content=np.loadtxt('/content/velocities.txt')
-x=content[:,0]
-f=content[:,1]
-distance=np.zeros(len(x))
-for i in range(1,len(x)):
-  base=0.5*(x[i]-x[i-1])
-  area=base*(f[i]+f[i-1])
-  distance[i]=distance[i-1]+area
+f=content[:,0]
+t=content[:,1]
+distance=np.zeros(len(t))
+for k in range(1,len(t)):
+  h=(t[k]-t[k-1])
+  area=0.5*h*(f[k]+f[k-1])
+  distance[k]=disatnce[k-1]+area
+fig,axes=plt.subplot(2,1,1)
 print(distance[-1])
-fig,axes=plt.subplots(2,1)
-axes[0].plot(x,f)
-axes[0].plot(x,distance)
+axes[0].plot(t,f)
+axes[1].plot(t,distance)
+plt.show()
